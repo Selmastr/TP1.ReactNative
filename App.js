7ced4b552d3cbe73aa4097e5aa93b50ee39c7831
Selmastr <sostrowiecki@ensc.fr>
2023-02-03 14:44:49 +0100

    partie 1

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style={styles.image} source={{uri:'https://www.bpesquet.fr/images/ENSC_2022.jpg'}} />
+      <Image style={styles.image} source={{uri:'https://www.bpesquet.fr/images/ENSC_2022.jpg'}} />
+      <Image style={styles.image} source={{uri:'https://www.bpesquet.fr/images/ENSC_2022.jpg'}} />
     </View>
-  );
+// AFFICHAGE DU TITRE HELLO WORLD
+//     // <View style={styles.container}>
+//     //   <Text style={styles.titre}>Hello World ;)</Text>
+//     //   <StatusBar style="auto" />
+//     // </View>
+// AFFICHAGE D'UN BLOC
+//     // <View style={styles.container}>
+//     //   <View style={{width: 100,    height: 100,    backgroundColor:"red",  }} />
+//     //   <View style={{  width: 100,    height: 100,    backgroundColor:"orange",  }} />
+//     //   <View style={{    width: 100,    height: 100,    backgroundColor:"green",  }} />
+//     // </View>
+   );
 }
 
 const styles = StyleSheet.create({
+  //alignement vertical des cubes
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#fff',
+  //   alignItems: 'center',
+  //   justifyContent: 'space-around',
+  // },
+  //alignement horizontal des cubes
+  // container: {
+  //   flex: 1,
+  //   flexDirection: "row",
+  //   backgroundColor: '#fff',
+  //   alignItems: 'center',
+  //   justifyContent: 'flex-start',
+  //   alignItems: "flex-start",
+  //   marginTop:60,
+  // },
+  // affichage des 3 images
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems : "flex-end",
+    justifyContent:"space-between",
+    paddingTop : 60,
   },
+ 
+  titre: {
+    color : "red",
+    fontWeight : "bold",
+    fontSize : 30,
+  },
+  image: {
+    width:143,
+    height: 45,
+  }
+
 });
